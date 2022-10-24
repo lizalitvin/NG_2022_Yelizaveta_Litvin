ba85b4f87233942566059d5a5f6fe1fb61fcc3b5
Your Name <you@example.com>
2022-10-24 20:53:13 +0300

    jjjjj

---
+++ exercise1.py
@@ -0,0 +1,16 @@
+what=input ("What are we doing? (+, -, *, / :)")
+
+a = float(input("Number One"))
+b = float(input("Number Two"))
+
+if what == "+":
+    print ("Result:", a+b)
+    c = a + b 
+elif what == "-":
+    print ("Result:", a-b)
+elif what == "*":
+   print ("Result:", a*b)
+elif what == "/":
+    print ("Result:", a/b)
+
+
